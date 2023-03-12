3a67e5c40dcb48bcb1d08472c050d3069d42dc42
Dixon1323 <dixonsunny1323@gmail.com>
2023-03-13 01:42:17 +0530

    Update esc_controll.ino

--- esc_controll/esc_controll.ino
+++ esc_controll/esc_controll.ino
@@ -1,8 +1,11 @@
 #include <Servo.h>
 Servo esc_signal;
+int a;
 
 void setup()
 {
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
   esc_signal.attach(9);  //Specify here the pin number on which the signal pin of ESC is connected.
   esc_signal.write(30);   //ESC arm command. ESCs won't start unless input speed is less during initialization.
   delay(3000);            //ESC initialization delay.
@@ -10,6 +13,7 @@ void setup()
 
 void loop()
 {
+  
 esc_signal.write(50);	  //Vary this between 40-130 to change the speed of motor. Higher value, higher speed.
 delay(15);
 }
